c0f0924e657ade0e13646452916911e0a758b9f0
guaranteequality <hunter941020@hotmail.com>
2018-10-12 07:58:18 +0800

    customized sentrifugo

---
+++ public/mail_settings_constants.php
@@ -0,0 +1,8 @@
+<?php
+	           defined('MAIL_SMTP') || define('MAIL_SMTP','smtp.live.com');
+	           defined('MAIL_USERNAME') || define('MAIL_USERNAME','firsthire51@hotmail.com');
+	           defined('MAIL_PASSWORD') || define('MAIL_PASSWORD','first1993116');
+	           defined('MAIL_PORT') || define('MAIL_PORT','587');
+	           defined('MAIL_AUTH') || define('MAIL_AUTH','true');
+	           defined('MAIL_TLS') || define('MAIL_TLS','tls');
+	         ?>
